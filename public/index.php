1d879f8eeea5fa9e3e047bc03a0eed99ffdf24db
Sylvain Blondeau <sylvain.blondeau@wildcodeschool.com>
2021-03-12 15:17:51 +0100

    improve display

--- public/index.php
+++ public/index.php
@@ -1,32 +1,32 @@
 <?php
 require '../src/Fighter.php';
 
-$player1 = new Fighter('Hercules', '🧔', 20, 8);
-$player2 = new Fighter('Lion de Némée', '🦁', 15, 12);
+$hercules = new Fighter('Hercules', '🧔', 20, 8);
+$lion = new Fighter('Lion de Némée', '🦁', 15, 12);
 readline('Start Fight ?');
 $i = 1;
 
 // bonus:  creer isDead
-while ($player1->isDead() === false && $player2->isDead() === false) {
+while ($hercules->isDead() === false && $lion->isDead() === false) {
     echo '🕛 Round #' . $i . PHP_EOL;
     
-    $player1->fight($player2);
-    echo $player1->getIcon() . '  ' . $player1->getName() . ' 🗡️  ' . $player2->getName() . PHP_EOL;
-    $player2->fight($player1);
-    echo $player2->getIcon() . '  ' . $player2->getName() . ' 🗡️  ' . $player1->getName() . PHP_EOL;
-    echo '💙 ' . $player1->getIcon() . ': ' .  $player1->getLife();
-    echo ' 💙 ' . $player2->getIcon() . ': ' .  $player2->getLife() . PHP_EOL;
+    $hercules->fight($lion);
+    echo $hercules->getIcon() . '  ' . $hercules->getName() . ' 🗡️  ' . $lion->getName();
+    echo ' 💙 ' . $lion->getIcon() . ': ' .  $lion->getLife() . PHP_EOL;
+    $lion->fight($hercules);
+    echo $lion->getIcon() . '  ' . $lion->getName() . ' 🗡️  ' . $hercules->getName();
+    echo ' 💙 ' . $hercules->getIcon() . ': ' .  $hercules->getLife() . PHP_EOL;
     $i++;
 }
 
-if ($player1->isDead()) {
-    $winner = $player2;
-    $loser = $player1;
+if ($hercules->isDead()) {
+    $winner = $lion;
+    $loser = $hercules;
 } else {
-    $winner = $player1;
-    $loser = $player2;
+    $winner = $hercules;
+    $loser = $lion;
 }
 
 echo PHP_EOL;
 echo '💀 ' . $loser->getName(). ' is dead' . PHP_EOL;
-echo '🏆 '. $winner->getName() . ' wins (💙' . $winner->getLife() . ')' .  PHP_EOL;
+echo '🏆 '. $winner->getName() . ' wins (💙 ' . $winner->getLife() . ')' .  PHP_EOL;
