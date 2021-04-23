c29212287d0d45f0dd4c0c5d2a55b2e7dd620e62
Sylvain Blondeau <sylvain.blondeau@wildcodeschool.com>
2021-04-23 15:37:24 +0200

    fix pb coordinates map

--- public/map.php
+++ public/map.php
@@ -1,28 +1,20 @@
 <?php if (isset($arena)) : ?>
     <div class="map" style="--tiles-number: <?= $arena->getSize() ?>">
-    <?php
-
-    for ($x = 0; $x < $arena->getSize(); $x++) : ?>
         <?php for ($y = 0; $y < $arena->getSize(); $y++) : ?>
-            <div>
-                <?php foreach ($arena->getMonsters() as $fighter) : ?>
-                    <?php if ($fighter->getX() === $x && $fighter->getY() === $y) : ?>
-                        <img
-                         title="Distance to Héraclès = <?= method_exists($arena, 'getDistance') ? $arena->getDistance($arena->getHero(), $fighter) : '' ?>" 
-                         class="monster <?= method_exists($arena, 'touchable') && $arena->touchable($arena->getHero(), $fighter)  ? 'touchable' : 'untouchable' ?>" 
-                         src="<?= $fighter->getImage(); ?>" 
-                         alt=""
-                        >
+            <?php for ($x = 0; $x < $arena->getSize(); $x++) : ?>
+                <div>
+                    <?php foreach ($arena->getMonsters() as $fighter) : ?>
+                        <?php if ($fighter->getX() === $x && $fighter->getY() === $y) : ?>
+                            <img title="Distance to Héraclès = <?= method_exists($arena, 'getDistance') ? $arena->getDistance($arena->getHero(), $fighter) : '' ?>" class="monster <?= method_exists($arena, 'touchable') && $arena->touchable($arena->getHero(), $fighter)  ? 'touchable' : 'untouchable' ?>" src="<?= $fighter->getImage(); ?>" alt="">
+                        <?php endif; ?>
+                    <?php endforeach; ?>
+                    <?php if ($arena->getHero()->getX() === $x && $arena->getHero()->getY() === $y) : ?>
+                        <img src="<?= $arena->getHero()->getImage(); ?>" alt="">
                     <?php endif; ?>
-                <?php endforeach; ?>
-                <?php if ($arena->getHero()->getX() === $x && $arena->getHero()->getY() === $y) : ?>
-                    <img src="<?= $arena->getHero()->getImage(); ?>" alt="">
-                <?php endif; ?>
-
-            </div>
+                </div>
+            <?php endfor; ?>
         <?php endfor; ?>
-    <?php endfor; ?>
-</div>
+    </div>
 <?php else : ?>
-<p>Add fighters in Arena</p>
+    <p>Add fighters in Arena</p>
 <?php endif; ?>
